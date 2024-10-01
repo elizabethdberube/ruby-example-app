640f97cadaa147b4884ceb4221d7418824286d7a
Lizeroni <liz@durableprogramming.com>
2024-10-01 15:51:31 -0400

    added to back buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 class ArticlesController < ApplicationController
- 
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
     end
 
     def create
-        @article = Article.new(article_params)
-        if @article.save
-            redirect_to @article
-          else
-            render 'new'
-          end
+      @article = Article.new(article_params)
+
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
     end
 
     def show
