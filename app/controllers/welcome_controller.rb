bb769d2181b5c6081accfc4a199d83b12f56635c
Lizeroni <liz@durableprogramming.com>
2024-10-03 13:54:53 -0400

    changed css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:title, :text])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+   
   get 'welcome/index'
 
-  
+  get '/welcome/tag/:title', to: 'welcome#tag#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
