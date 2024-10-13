7793fd76f971452afe87bb50ee34e4172d935b04
Lizzy <elizabethdberube@gmail.com>
2024-10-13 12:48:25 -0400

    search bar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,41 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
         @user = current_user.name
+
+            
+          if params[:search_by_title] && params[:search_by_title] != ""
+            @results = @articles.where("title like ?", 
+            "%# {params[:search_by_title]}%")
+          end
+          if params[:search_by_user_name] && params[:search_by_user_name] != ""
+            @results = @articles.where("user like ?", 
+            "%# {params[:search_by_user_name]}%")
+          end
+         if params[:search_by_date] && params[:search_by_date] != ""
+            @results = @articles.where("date like ?", 
+            "%# {params[:search_by_date]}%")
+          end
+       
+    end
+
+        def index
+        @articles = Article.all
+        @user = current_user.name
+
+            
+          if params[:search_by_title] && params[:search_by_title] != ""
+            @articles = @articles.where("title like ?", 
+            "%# {params[:search_by_title]}%")
+          end
+          if params[:search_by_user_name] && params[:search_by_user_name] != ""
+            @articles = @articles.where("user like ?", 
+            "%# {params[:search_by_user_name]}%")
+          end
+         if params[:search_by_date] && params[:search_by_date] != ""
+            @articles = @articles.where("date like ?", 
+            "%# {params[:search_by_date]}%")
+          end
+       
     end
 
     def new
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-
    
   get 'welcome/index'
 
